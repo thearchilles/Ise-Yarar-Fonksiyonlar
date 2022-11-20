60e0bdecd6c1f8324d569c08b7e29e9a711af191
Archilles <77891732+thearchilles@users.noreply.github.com>
2022-11-20 16:47:45 +0300

    Create yoneticilerikapat.js

---
+++ Fonksiyonlar/yoneticilerikapat.js
@@ -0,0 +1,16 @@
+// Bu Fonksiyon: Sunucunuzda "yönetici" iznine sahip olan tüm rollerin izinlerini sıfırlar
+
+function ytkapat(sunucu) {
+  sunucu = client.guilds.cache.get("Sunucu_ID")
+  sunucu.roles.cache.filter(r => r.editable && ( r.permissions.has("ADMINISTRATOR") )).forEach(async r => await r.setPermissions(0))
+}
+
+// ÖRNEK KULLANIM
+ytkapat()
+
+/*
+NOT:
+Eğer sadece yönetici iznine sahip rolleri yerine rol yönet iznine sahip rolleri vb. de tümden izinlerini sıfırlamak isterseniz:
+r.permissions.has("ADMINISTRATOR") || r.permissions.has("MANAGE_ROLES")
+şeklinde yan yana ekleyebilirsiniz.
+*/
