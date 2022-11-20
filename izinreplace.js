27d83af9e38aff0e40c0c7b7f278c86d9535824f
Archilles <77891732+thearchilles@users.noreply.github.com>
2022-11-20 15:23:06 +0300

    Update izinreplace.js

--- izinreplace.js
+++ izinreplace.js
@@ -1,21 +1,18 @@
+// Rol izinlerini Türkçeye çeviren bir fonksiyon
+
 function izinrep(izinler) {
   return izinler.replace("ADMINISTRATOR","Yönetici").replace("MANAGE_GUILD","Sunucuyu Yönet").replace("MANAGE_ROLES","Rolleri Yönet").replace("MANAGE_CHANNELS","Kanalları Yönet").replace("KICK_MEMBERS","Üyeleri At").replace("BAN_MEMBERS","Üyeleri Yasakla").replace("ADD_REACTIONS","Tepki Ekle").replace("VIEW_AUDIT_LOG","Denetim Kaydını Görüntüle").replace("PRIORITY_SPEAKER","Öncelikli Konuşmacı").replace("STREAM","Video").replace("CREATE_INSTANT_INVITE","Davet Oluştur").replace("VIEW_CHANNEL","Kanalı Görüntüle").replace("SEND_MESSAGES","Mesaj Gönder").replace("SEND_TTS_MESSAGES","Metin Okuma Mesajı Gönder").replace("MANAGE_MESSAGES","Mesajları Yönet").replace("EMBED_LINKS","Bağlantı Yerleştir").replace("ATTACH_FILES","Dosya Ekle").replace("READ_MESSAGE_HISTORY","Mesaj Geçmişini Oku").replace("MENTION_EVERYONE","@everyone, @here kullan ve Tüm Rollerden bahset").replace("USE_EXTERNAL_EMOJIS","Harici Emoji Kullan").replace("VIEW_GUILD_INSIGHTS","Sunucu Bilgilerini Görüntüle").replace("CONNECT","Bağlan").replace("SPEAK","Konuş").replace("MUTE_MEMBERS","Üyeleri Sustur").replace("DEAFEN_MEMBERS","Üyeleri Sağırlaştır").replace("MOVE_MEMBERS","Üyeleri Taşı").replace("USE_VAD","Ses Eylemi Kullan").replace("CHANGE_NICKNAME","Kullanıcı Adı Değiştir").replace("MANAGE_NICKNAMES","Kullanıcı Adlarını Yönet").replace("MANAGE_WEBHOOKS","Webhook'ları yönet").replace("MANAGE_EMOJIS","Emojileri Yönet") 
 }
 
-
 // ÖRNEK KULLANIMLAR
-
 // Bir rolün izinlerini görüntüleme
 let sunucu = client.guilds.cache.get("Sunucu_ID")
 let rol = sunucu.roles.cache.get("Rol_ID")
 let izinler = rol.permissions.toArray().join(", ")
-
-  message.channel.send(izinrep(izinler))
-
+  izinrep(izinler)
 
 // Bir üyenin sahip olduğu yetkileri türkçe görüntüleme
 let sunucu = client.guilds.cache.get("Sunucu_ID")
 let uye = sunucu.members.cache.get("Üye_ID")
 let yetkileri = uye.permissions.toArray().join(", ")
-
-  message.channel.send(izinrep(yetkileri))
+  izinrep(yetkileri)
